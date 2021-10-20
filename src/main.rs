284df24f364c9756becdc482441ee4fdd47e9655
iLePix <elias.hanelt@gmail.com>
2021-10-20 21:49:06 +0200

    Added Perlin noise world gen

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ use sdl2::rect::{Point, Rect};
 use vecm::vec::Vec2u;
 use std::time::Duration;
 
+mod world; 
+
+use world::*;
+
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("Algos", 800, 600)
+    let window = video_subsystem.window("Algos", 1600, 1000)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
@@ -18,30 +22,38 @@ fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build()
         .expect("could not make a canvas");
     
-    let world_size = Vec2u::new(16, 12);
+    let world_size = Vec2u::new(80, 50);
+    let tile_size = 20; 
     let mut mouse_pos = Vec2u::zero();
-    let screen_size = Vec2u::new(800, 600);    
+    let screen_size = Vec2u::new(1600, 1000);    
 
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
+
+    let mut world = World::new(world_size);
+    world.p();
+
+
     'running: loop {
         i = (i + 1) % 80;
         //canvas.set_draw_color(Color::RGB(0, 0, 0));
 
-    
+        
 
-        for x in 0..world_size.x {
-            for y in 0..world_size.y {
-                if mouse_pos.x >= (x*50) && mouse_pos.x < (x*50 + 50) && mouse_pos.y >= (y*50) && mouse_pos.y < (y*50 + 50){ 
-                    canvas.set_draw_color(Color::RGB((x*8) as u8, (y*8) as u8, 0));
-                } else {
-                    canvas.set_draw_color(Color::RGB(0, 0, 0));
+        for x in 0..world.size.x {
+            for y in 0..world.size.y {
+                if mouse_pos.x >= (x*tile_size) && mouse_pos.x < (x*tile_size + tile_size) && mouse_pos.y >= (y*tile_size) && mouse_pos.y < (y*tile_size + tile_size){ 
+                    canvas.set_draw_color(Color::RGB(255 , 0, 0));
+                } else if world.get_tile(Vec2u::new(x, y)) == Tile::Land {
+                    canvas.set_draw_color(Color::RGB(0, 128, 0));
+                } else if world.get_tile(Vec2u::new(x, y)) == Tile::Water {
+                    canvas.set_draw_color(Color::RGB(0, 0, 128));
                 }
                 
-                canvas.fill_rect(Rect::new((x * 50) as i32, (y * 50) as i32, 50, 50))?;
+                canvas.fill_rect(Rect::new((x * tile_size) as i32, (y * tile_size) as i32, tile_size, tile_size))?;
             }
         }
 
--- src/world/mod.rs
+++ src/world/mod.rs
@@ -1,6 +1,47 @@
-use vecm::*;
+extern crate perlin_noise as perlin;
+use vecm::vec::*;
+use perlin::PerlinNoise;
 
-struct World {
-    size: Vec2u,
-    //[[Tile; X_SIZE]; Y_SIZE]
+pub struct World {
+    pub size: Vec2u,
+    tiles: Vec<Tile>
+}
+
+impl World {
+    pub fn new(size: Vec2u) -> Self {
+        Self {size, tiles: vec![Tile::Land; (size.x * size.y) as usize]}
+    }
+
+    pub fn p(&mut self) {
+        let perlin = PerlinNoise::new();
+        let plus = 0.06;
+        for x in 0..self.size.x {
+            for y in 0..self.size.y {
+                let height = perlin.get2d([x as f64 + 0.74, y as f64 + 0.74]);
+                if height > 0.5 {
+                    self.set_tile(Vec2u::new(x, y), Tile::Water);
+                } else {
+                    self.set_tile(Vec2u::new(x, y), Tile::Land);
+                }
+            }
+        }
+    }
+
+    pub fn get_tile(&self, pos: Vec2u) -> Tile {
+        assert!(pos.x < self.size.x && pos.y < self.size.y);
+        self.tiles[(pos.x * self.size.y + pos.y) as usize]
+    }
+
+    pub fn set_tile(&mut self, pos: Vec2u, tile: Tile) {
+        assert!(pos.x < self.size.x && pos.y < self.size.y);
+        self.tiles[(pos.x * self.size.y + pos.y) as usize] = tile;
+    }
+}
+
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum Tile {
+    Land,
+    Water,
+    Selected
 }
